5676deaff442f7126265d9953a38e01a1bf3eea3
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-06-13 11:33:00 -0300

    :wrench: Upgrades compose version (#15)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins{
 }
 
 buildscript {
-    val compose_version by extra("1.0.0")
+    val compose_version by extra("1.1.0")
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.0.0")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.15.1")
     }
--- compose-sample/build.gradle.kts
+++ compose-sample/build.gradle.kts
@@ -49,14 +49,12 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-        useIR = true
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
-        kotlinCompilerVersion = "1.5.10"
     }
     packagingOptions {
         resources {
