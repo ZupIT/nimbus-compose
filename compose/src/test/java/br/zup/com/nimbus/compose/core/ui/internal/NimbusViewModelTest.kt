7105a5129ffda97e09fb19ba089566d5fcda88d8
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-06-24 14:17:02 -0300

    :white_check_mark: Add another test

--- compose/src/test/java/br/zup/com/nimbus/compose/core/ui/internal/NimbusViewModelTest.kt
+++ compose/src/test/java/br/zup/com/nimbus/compose/core/ui/internal/NimbusViewModelTest.kt
@@ -1,5 +1,6 @@
 package br.zup.com.nimbus.compose.core.ui.internal
 
+import app.cash.turbine.FlowTurbine
 import app.cash.turbine.test
 import br.zup.com.nimbus.compose.core.ui.internal.util.CoroutinesTestExtension
 import br.zup.com.nimbus.compose.core.ui.internal.util.RandomData
@@ -11,6 +12,7 @@ import com.zup.nimbus.core.render.Renderer
 import com.zup.nimbus.core.render.ServerDrivenView
 import com.zup.nimbus.core.tree.ServerDrivenNode
 import io.mockk.Runs
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
@@ -23,6 +25,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.lang.RuntimeException
 
 @ExperimentalCoroutinesApi
 @ExtendWith(CoroutinesTestExtension::class)
@@ -65,7 +68,7 @@ class NimbusViewModelTest : BaseTest() {
             //When
             viewModel.initFirstViewWithRequest(viewRequest)
 
-            val page = pageManagerAddSlot.first()
+            val page = pageManagerAddSlot.last()
 
             emitOnChangeServerDrivenNode(serverDrivenNode)
 
@@ -74,6 +77,65 @@ class NimbusViewModelTest : BaseTest() {
                 assertEquals(expectedFirstEmission, awaitItem())
                 assertEquals(expectedSecondEmission, awaitItem())
             }
+        }
+
+        @DisplayName("Then should post PageStateOnLoading and PageStateOnError then retry with success")
+        @Test
+        @Suppress("UNCHECKED_CAST")
+        fun testGivenAViewRequestWhenInitFirstViewShouldPageStateOnError() = runTest {
+
+            // Given
+            val viewRequest = ViewRequest(url = RandomData.httpUrl())
+            val expectedException = RuntimeException("Any Exception")
+            val expectedLoading = NimbusPageState.PageStateOnLoading
+            val expectedPageError = NimbusPageState.PageStateOnError(throwable = expectedException, retry = {})
+            val expectedOnShowPage = NimbusPageState.PageStateOnShowPage(serverDrivenNode)
+
+
+            coEvery { nimbusConfig.core.viewClient.fetch(any()) } throws expectedException
+
+            //When
+            viewModel.initFirstViewWithRequest(viewRequest)
+
+            val page = pageManagerAddSlot.last()
+
+            //Then
+            page.content.test {
+                val secondItem = shouldBeLoadingAndError(expectedLoading, expectedPageError)
+
+                shouldBeLoadingAndShowPage(secondItem, expectedLoading, expectedOnShowPage)
+            }
+        }
+
+        private suspend fun FlowTurbine<NimbusPageState>.shouldBeLoadingAndShowPage(
+            secondItem: NimbusPageState.PageStateOnError,
+            expectedLoading: NimbusPageState.PageStateOnLoading,
+            expectedOnShowPage: NimbusPageState.PageStateOnShowPage,
+        ) {
+            coEvery { nimbusConfig.core.viewClient.fetch(any()) } returns renderNode
+
+            //Simulates user clicking retry button on error screen
+            secondItem.retry.invoke()
+            emitOnChangeServerDrivenNode(serverDrivenNode)
+
+            val thirdItem = awaitItem()
+            assertEquals(expectedLoading, thirdItem)
+
+            val fourthItem = awaitItem()
+            assertEquals(expectedOnShowPage, fourthItem)
+        }
+
+        private suspend fun FlowTurbine<NimbusPageState>.shouldBeLoadingAndError(
+            expectedLoading: NimbusPageState.PageStateOnLoading,
+            expectedPageError: NimbusPageState.PageStateOnError,
+        ): NimbusPageState.PageStateOnError {
+            val firstItem = awaitItem()
+            assertEquals(expectedLoading, firstItem)
+
+            val secondItem = (awaitItem() as NimbusPageState.PageStateOnError)
+            assertEquals(expectedPageError.throwable,
+                secondItem.throwable)
+            return secondItem
         }
     }
 
