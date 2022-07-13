b2e3c95d1669238f30a234f9541c8a062790b07c
Tiago Peres França <tiago.franca@zup.com.br>
2022-07-13 16:00:05 -0300

    Update build.gradle.kts

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -11,7 +11,7 @@ val serializationVersion = "1.3.2"
 val ktorVersion = "1.6.8"
 
 dependencies {
-    api("br.com.zup.nimbus:nimbus-core-android:1.0.0-alpha4")
+    api("br.com.zup.nimbus:nimbus-core-android:1.0.0-alpha5")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
