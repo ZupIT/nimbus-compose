9e2b889b91626282eca5001916804dfb166de16c
Tiago Peres França <tiago.franca@zup.com.br>
2022-08-03 15:49:39 -0300

    fixes extra recompositions

--- compose/src/main/java/br/zup/com/nimbus/compose/ComponentData.kt
+++ compose/src/main/java/br/zup/com/nimbus/compose/ComponentData.kt
@@ -1,10 +1,22 @@
 package br.zup.com.nimbus.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import com.zup.nimbus.core.tree.ServerDrivenNode
 
+@Immutable
 class ComponentData(
     val node: ServerDrivenNode,
     val parent: ServerDrivenNode?,
     val children: @Composable () -> Unit,
-)
+) {
+    private val hash: Int = node.properties?.hashCode() ?: 0
+
+    override fun hashCode(): Int {
+        return hash
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is ComponentData) other.hashCode() == hash else false
+    }
+}
