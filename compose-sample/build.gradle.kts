99dec2f2e59a1a5ce651adcf779ed680d0a55414
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-10-26 17:58:01 -0300

    upgrading navigation-compose version to fix popToView crash
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- compose-sample/build.gradle.kts
+++ compose-sample/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("kotlin-android")
     id("de.mannodermaus.android-junit5")
     id("org.jetbrains.dokka")
-    id("com.google.devtools.ksp") version "1.6.10-1.0.4"
+    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
     //idea
 }
 
