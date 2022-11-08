93b5466cd62b624b0a968b24642913c413ba1699
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-11-08 16:34:45 -0300

    Extract modal state
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- compose/src/main/java/br/zup/com/nimbus/compose/internal/NimbusNavHost.kt
+++ compose/src/main/java/br/zup/com/nimbus/compose/internal/NimbusNavHost.kt
@@ -72,6 +72,7 @@ internal fun NimbusNavHost(
                     {
                         modalParentHelper.triggerAnimatedClose()
                     })
+
                     page.Compose()
 
                     nimbusViewModelModalState.HandleModalState(
