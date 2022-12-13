8c4c8b3339ba19093d284a8f16ea1dbb43060ab4
Tiago Peres França <tiago.franca@zup.com.br>
2022-12-13 10:19:18 -0300

    centralizes the loading component

--- compose/src/main/java/br/com/zup/nimbus/compose/ui/components/DefaultComponents.kt
+++ compose/src/main/java/br/com/zup/nimbus/compose/ui/components/DefaultComponents.kt
@@ -16,16 +16,25 @@
 
 package br.com.zup.nimbus.compose.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 internal fun LoadingDefault(modifier: Modifier = Modifier) {
-    CircularProgressIndicator(modifier = modifier)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        CircularProgressIndicator(modifier = modifier)
+    }
 }
 
 @Composable
